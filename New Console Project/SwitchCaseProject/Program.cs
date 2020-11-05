4c1e0ba395a4df645b3f1147c6d805adf23d27e1
Hakan-indp <68620379+Hakan-indp@users.noreply.github.com>
2020-11-05 03:45:10 +0300

    Add files via upload

---
+++ New Console Project/SwitchCaseProject/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwitchCaseProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string plaka;
+            Console.WriteLine("Lütfen plaka numaranızı giriniz");
+            plaka = Console.ReadLine();
+            switch (plaka)
+
+            {
+                case "33":
+                    Console.WriteLine("33 Mersin");
+                    break;
+                case "34":
+                    Console.WriteLine("34 İstanbul");
+                    break;
+                case "01":
+                    Console.WriteLine("01 Adana");
+                    break;
+                default:
+                    Console.WriteLine("Hatalı giriş yaptınız!");
+                    break;
+                    Console.ReadKey();
+
+
+
+
+            }
+
+
+
+
+
+
+        }
+    }
+}
---
+++ New Console Project/YeniKonsolProjesi/YeniKonsolProjesi/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YeniKonsolProjesi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double boy, kilo, vki;
+            Console.WriteLine("Lütfen kilonuzu giriniz");
+            kilo = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Lütfen boyunuzu metre cinsinden giriniz");
+            boy = Convert.ToDouble(Console.ReadLine());
+            vki = kilo / (boy * boy);
+            if (vki<= 18.4)
+            {
+                Console.WriteLine("Vücut kitle endeksi:{0} kategori:zayıf", vki);
+            }
+
+            else if (vki<= 24.9)
+            {
+                Console.WriteLine("vücut kitle endeksi:{0} kategori: orta", vki);
+            }
+
+            else if (vki<= 29.9)
+            {
+                Console.WriteLine("vücut kitle endeksi:{0} kategori: kilolu", vki);
+            }
+            else
+            {
+                Console.WriteLine("bilgileri yanlış girdiniz");
+            }
+
+            Console.ReadKey();
+
+    }
+
+           
+    }
+}
